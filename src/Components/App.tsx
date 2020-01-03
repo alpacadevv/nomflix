fec21cd7cd2e1ef13ccc110f2acf2a387a48425e
Lee Jinseop <js428118@gmail.com>
2020-01-03 16:04:19 +0900

    ohoho

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -10,14 +10,15 @@ import Language from './Context'
 
 import translations from './translations'
 
-import ReactGA from 'react-ga'
-
-ReactGA.initialize('UA-155258262-1')
+import Helmet from 'react-helmet'
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
+      <Helmet>
+        <script dangerouslySetInnerHTML={{ __html: `alert('??')` }}></script>
+      </Helmet>
       <Router />
       <Language defaultLanguage="en" translations={translations}>
         <Screen />
