f2a24c00a44ea6383a7f1e593cbcd9de70a60f44
Lee Jinseop <js428118@gmail.com>
2020-01-03 17:17:41 +0900

    modify ga

--- src/Components/Meta.tsx
+++ src/Components/Meta.tsx
@@ -13,34 +13,42 @@ const Meta: FC<MetaProps> = ({ title }) => {
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155258262-1"></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'UA-155258262-1');
-            `,
+            __html: `window.dataLayer = window.dataLayer || [];
+
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+      
+          gtag('config', 'UA-155258262-1');`,
           }}
         />
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WHC7BKH');
-            `,
-          }}
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHC7BKH" height="0" width="0" style="display:none;visibility:hidden;"></iframe>
-            `,
+            __html: `(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+              'gtm.start': new Date().getTime(),
+              event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-WHC7BKH');`,
           }}
         />
       </Helmet>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHC7BKH" height="0" width="0" style="display:none;visibility:hidden;"></iframe>
+            `,
+        }}
+      />
     </>
   )
 }
