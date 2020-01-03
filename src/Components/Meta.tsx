5f80f1c15f13159ca1f51a450dc7a8e7fed9e234
Lee Jinseop <js428118@gmail.com>
2020-01-03 16:34:31 +0900

    sd

--- src/Components/Meta.tsx
+++ src/Components/Meta.tsx
@@ -17,14 +17,14 @@ const Meta: FC<MetaProps> = ({ title }) => {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MC9X84N');
+              })(window,document,'script','dataLayer','GTM-WHC7BKH');
             `,
           }}
         />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC9X84N" height="0" width="0" style="display:none;visibility:hidden;"></iframe>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHC7BKH" height="0" width="0" style="display:none;visibility:hidden;"></iframe>
             `,
           }}
         />
